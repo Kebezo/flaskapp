873b6cf91df0ed99b6cfbc678f6b7e744f7e2fce
Kebezo <112317873+Kebezo@users.noreply.github.com>
2022-08-29 12:16:50 +0200

    spre

--- market/__init__.py
+++ market/__init__.py
@@ -6,8 +6,8 @@ from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hnxfwzdteurpuf:3d311ecbb5aab7f6f61eb0919ad9418a48a97321c067fce4fb0546c017957554@ec2-34-234-240-121.compute-1.amazonaws.com:5432/ddein4lv8a6lm7'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hnxfwzdteurpuf:3d311ecbb5aab7f6f61eb0919ad9418a48a97321c067fce4fb0546c017957554@ec2-34-234-240-121.compute-1.amazonaws.com:5432/ddein4lv8a6lm7'
 #randomly genereirano hexadecimal stevilo
 app.config['SECRET_KEY'] = '52627e432fe1def0e3834e53'
 bcrypt = Bcrypt(app)
