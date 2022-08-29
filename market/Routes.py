67cde916de2d23795c0cea0fd41e26d7614e765a
Kebezo <112317873+Kebezo@users.noreply.github.com>
2022-08-29 12:00:39 +0200

    added procfile

--- market/Routes.py
+++ market/Routes.py
@@ -6,6 +6,7 @@ from market import db
 from flask_login import login_user, logout_user, login_required, current_user
 import folium
 
+
 @app.route('/')
 @app.route('/home')
 def home_page():
--- market/__init__.py
+++ market/__init__.py
@@ -3,9 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
+
+
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kutycmqskxrzpc:0a53ecba63f91bc9ac1854054114c3522ac6f81115ca841f9e506b7b12538100@ec2-34-234-240-121.compute-1.amazonaws.com:5432/dbbos429tdhhb8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hnxfwzdteurpuf:3d311ecbb5aab7f6f61eb0919ad9418a48a97321c067fce4fb0546c017957554@ec2-34-234-240-121.compute-1.amazonaws.com:5432/ddein4lv8a6lm7'
 #randomly genereirano hexadecimal stevilo
 app.config['SECRET_KEY'] = '52627e432fe1def0e3834e53'
 bcrypt = Bcrypt(app)
